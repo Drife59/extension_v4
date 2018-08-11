e14dbd513db4e5129177aab8dfc7afac51b80285
GRASSART Benjamin <bjbtiben@hotmail.fr>
2018-08-11 11:47:47 +0200

    Fix error message when email is empty

--- src/Signin.js
+++ src/Signin.js
@@ -19,6 +19,11 @@ export class Signin extends Component {
         msg_wrong_email.style.display = "none";
         msg_wrong_password.style.display = "none";
 
+        if(email == "" || email == " "){
+            msg_wrong_email.style.display = 'block';
+            return false;
+        }
+
         if(password == "" || password == " "){
             msg_wrong_password.style.display = 'block';
             return false;
