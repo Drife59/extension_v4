2c5196c40cb5fc7d13a946c103d26911f4d42d34
bjbtiben@hotmail.fr <bjbtiben@hotmail.fr>
2018-12-29 13:06:28 +0100

    Implement main onChange algo

--- public/lib/onChange.js
+++ public/lib/onChange.js
@@ -12,32 +12,13 @@ Cf config manifest.json
 
 var OnChangeLogger = new Logger(CODE_CHANGE);
 
-//Algo if key is found in domain
-function pivotTrouveDomaine(current_user, pivot_domaine, valeur_utilisateur, cle_domaine){
-	user_front_db.change_value_pivot_trouve_domaine(cle_domaine, pivot_domaine, valeur_utilisateur);
-}
 
-//Cannot find key for domain
-function pivotNonTrouveDomaine(current_user, cle_domaine, valeur_utilisateur){
-	OnChangeLogger.log("Algo change / unknown pivot domain: cannot find pivot for key " + cle_domaine);
-
-	//Create new dummy pivot
-	var new_pivot = "pivot_" + guid();
-	var xhttp_dom = xhttp_add_pivot_domaine(cle_domaine, new_pivot);
-	
-	xhttp_dom.onreadystatechange = function () {
-		if (xhttp_dom.readyState == 4 && (xhttp_dom.status == 200 || xhttp_dom.status == 204)) {
-			OnChangeLogger.info("Algo change / unknown pivot domain: created pivot domain: " + cle_domaine + " -> " + new_pivot);
-			user_front_db.change_value_pivot_non_trouve_domaine(cle_domaine, new_pivot, valeur_utilisateur);
-		}
-	}
-}
 
 //Main algo for event change detected on input field
 function changeAlgo(evt){
 	var champ = evt.target
 	OnChangeLogger.log("Algo change: field " + champ.tagName + " modified: " + HtmlEltToString(champ));
-	var cle_domaine = construit_domaine_cle(champ);
+	var key_domain = construit_domaine_cle(champ);
 	var valeur_utilisateur = champ.value.capitalize();
 
 	//Don't normalize email or password field
@@ -70,19 +51,45 @@ function changeAlgo(evt){
 	//Get current user and launch algo
 	chrome.storage.sync.get("current_user", function(data) {
 		if( Object.keys(data).length !== 0){
-			var xhttp_dom = xhttp_get_cle_domaine_v2(cle_domaine);
-			xhttp_dom.onreadystatechange = function () {
-				//Key exist for domain
-				if (xhttp_dom.readyState == 4 && xhttp_dom.status == 200) {
-					var pivot_domaine = JSON.parse(xhttp_dom.responseText)["pivot"]["name"];
-					OnChangeLogger.log("Algo Change: Key " + cle_domaine + " known, corresponding to " + pivot_domaine);
-					pivotTrouveDomaine(data.current_user, pivot_domaine, valeur_utilisateur, cle_domaine);
-				}
-				//Cannot find key in domain
-				else if(xhttp_dom.readyState == 4 && xhttp_dom.status == 404) {
-					OnChangeLogger.log("Algo change: Cannot find pivot in domain");
-					pivotNonTrouveDomaine(data.current_user, cle_domaine, valeur_utilisateur);
-				}
+
+			var domain = window.location.host;
+
+			var key_object     = website_front_db.has_key(domain, key_domain);
+			var pivot_referent = website_front_db.get_referent_pivot(domain, key_domain);
+
+			console.log("Key object = " + JSON.stringify(key_object, null, 4));
+			console.log("pivot referent = " + pivot_referent);
+
+			//In all the three cases below, we update key pivot weight, using an objecf from user front db
+			/* pivot_weight is like below, representing pivot weight, for corresponding user value 
+				* {
+				*      "nom": "2.3",
+				*      "prenom": "4.5"
+				* }
+			*/
+			var pivot_weight = user_front_db.get_pivot_weight_from_values(valeur_utilisateur);
+
+			//Key exist and there is a referent pivot
+			if(key_object && website_front_db.get_referent_pivot(domain, key_domain) != null){
+				console.info("Key and pivot referent found: " + key_domain + ": " + pivot_referent);
+				user_front_db.change_value_pivot_trouve_domaine(key_domain, pivot_referent, valeur_utilisateur);		
+				website_front_db.apply_pivot_on_key(domain, key_domain, pivot_weight);
+			}
+			//Key exist but no pivot referent
+			else if(key_object){
+				console.log("No pivot referent for key domain " + key_domain);
+				//get pivot and weight associated with user value
+				website_front_db.apply_pivot_on_key(domain, key_domain, pivot_weight);
+			}
+			//Key does not exist at all
+			else{
+				console.log("No key at all for domain key: " + key_domain);
+				//heuristic weight is undefined
+				website_front_db.create_key(domain, key_domain)
+				website_front_db.apply_pivot_on_key(domain, key_domain, pivot_weight);
+
+				//Don't add user value for now
+				//user_front_db.change_value_pivot_non_trouve_domaine(key_domain, new_pivot, valeur_utilisateur);
 			}
 		}else{
 			OnChangeLogger.error("Algo change: Critical cannot find current user. Please log in.");
