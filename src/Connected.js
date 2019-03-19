403f0936e3ad5c3feb12fc66c67b1d01081c7151
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-03-19 10:24:15 +0100

    Implement clear button + call clear field

--- src/Connected.js
+++ src/Connected.js
@@ -4,13 +4,26 @@ import { FooterContact } from './FooterContact';
 
 export class Connected extends Component {
 
+    constructor(props){
+        super(props);
+        this.clearField = this.clearField.bind(this);
+    }
+
+    clearField(e){
+        window.console.info("clear field");
+
+        //alert(JSON.stringify(window.inputs, null, 4));
+        window.clear_inputs();
+    }
+
     render() {
         return(
             <section>
                 <div id="main" class="col-12">
                     <div id="voice">
                         <img alt="img_connected" id="img_connected" src="images/need_you.png" />
-                    </div>    
+                    </div>
+                    <button onClick={this.clearField}> clear </button>    
                     <p> Hey {this.props.getUser()}, we need you ! </p>
                 </div>
                 <FooterContact />
