7467085ecd9d41f498819bf75d89f8333e3d0441
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-03-20 15:44:22 +0100

    Smallfix on LogOut React component
    To avoid warning

--- src/logout.js
+++ src/logout.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    
-    constructor(props){
-        super(props);
-    }
 
     render() {
 
         return (
             <div id="menu" class="col-2">
                 <a href="signout">
-                    <img src="images/menu_icon.png" width="72%" />
+                    <img src="images/menu_icon.png" width="72%" alt="logout" />
                 </a>
             </div>
         );
