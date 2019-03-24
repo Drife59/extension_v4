6e01ff51411c3d491e81a3d9d39d553a5eb42005
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-03-24 12:14:37 +0100

    Move all front db file in dedicated dir

--- public/front_db/profil.js
+++ public/front_db/profil.js
@@ -281,4 +281,23 @@ class UserProfil {
         console.info("[get_value_for_pivot]: Could not find value in profil " + profil_id + " for pivot " + pivot_name);
         return false;
     }
+
+    //Return a string to display a profil for input list
+    get_display_string(profil_id){
+
+        //email is mandatory
+        var display_str = this.get_value_for_pivot(profil_id, "main_email");
+
+        //Try to add family name and first if available
+        var family_name = get_value_for_pivot(profil_id, "family_name");
+        if( family_name != null){
+            display_str += (" " + family_name);  
+        }
+
+        var first_name = get_value_for_pivot(profil_id, "first_name");
+        if( family_name != null){
+            display_str += (" " + first_name);  
+        }
+        return display_str;
+    }
 }
