1f1a75a9afac6ce5c239cebbc523a6573b50a225
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-03-27 14:16:44 +0100

    Re-indent graphical profil lib

--- public/lib/graphical_profil.js
+++ public/lib/graphical_profil.js
@@ -3,83 +3,126 @@ Projet Corail
 Auteur: Benjamin GRASSART
 Année: 2019
 
-user_db.js
+graphical_profil.js
 
 V6 functionnality.
 Define graphical function for profil.
 
 */
 
-//Define event to create lists on input to display profil
-function init_event_list(){
-    for (var i = 0; i < type_to_include.length; i++) {
-        var inputs_type = inputs[type_to_include[i]];
+//https://www.kirupa.com/html5/get_element_position_using_javascript.htm
+//Return absolute position of en element
+function getPosition(el) {
+	var xPos = 0;
+	var yPos = 0;
 
-        for (j = 0; j < inputs_type.length; j++) {
+	while (el) {
+		if (el.tagName == "BODY") {
+			// deal with browser quirks with body/window/document and page scroll
+			var xScroll = el.scrollLeft || document.documentElement.scrollLeft;
+			var yScroll = el.scrollTop || document.documentElement.scrollTop;
 
-            var current_input = inputs_type[j];
-            //Don't process search field
-            if (is_search_field(current_input)) {
-                continue;
-            }
+			xPos += (el.offsetLeft - xScroll + el.clientLeft);
+			yPos += (el.offsetTop - yScroll + el.clientTop);
+		} else {
+			// for all other non-BODY elements
+			xPos += (el.offsetLeft - el.scrollLeft + el.clientLeft);
+			yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+		}
 
-            //On hover, create and bind the list
-            current_input.onmouseover = function(evt){
-                evt.target.setAttribute("list", id_list);
-
-                //Just put the list once
-                if(evt.target.nextSibling.tagName != "DATALIST"){
-                    evt.target.after(buildProfilList());
-                }
-            };
-
-            //On mouse leave, delete the list and unbind it
-            /*current_input.onmouseleave = function(evt){
-                evt.target.removeAttribute("list");
-
-                console.log("Type of next sibling: " + evt.target.nextSibling.tagName);
-                //Just put the list once
-                if(evt.target.nextSibling.tagName == "DATALIST"){
-                    evt.target.nextSibling.remove();
-                }
-            };*/
-        }
-    }
+		el = el.offsetParent;
+	}
+	return {
+		x: xPos,
+		y: yPos
+	};
 }
 
-//Build html to show a list (datalist HTML5)
+
+//Define event to create lists on input to display profil
+function init_event_list() {
+
+	//We absolutely need to position the block at body root, in order 
+	//to have the profil list properly positionned.
+	//Profil list will have a position absolute, calculated from input position
+	var list_profil = buildProfilList();
+	window.document.body.appendChild(list_profil)
+
+	for (var i = 0; i < type_to_include.length; i++) {
+		var inputs_type = inputs[type_to_include[i]];
+
+		for (j = 0; j < inputs_type.length; j++) {
+
+			//Don't process search field
+			if (is_search_field(inputs_type[j])) {
+				continue;
+			}
+
+			//On hover, create and bind the list
+			inputs_type[j].onmouseover = function (evt) {
+
+				var position_current_input1 = getPosition(evt.target);
+				console.debug("Position of the current input (1). x: " + position_current_input1.x + " y: " + position_current_input1.y);
+
+				//Display list as block, resize and position it
+				list_profil.style.display = "block";
+
+				//Same size as the field
+				var str_style = "width:" + evt.target.offsetWidth + "px;";
+				str_style += "left: " + position_current_input1.x + "px;";
+				//Vertical position: original input position + vertical size input + vertical scroll
+				str_style += "top: " + (position_current_input1.y + evt.target.offsetHeight + window.scrollY) + "px;";
+				str_style += "position: absolute;";
+
+				list_profil.setAttribute("style", str_style);
+				console.debug("List position was set to absolute: " + list_profil.style.left + " / " + list_profil.style.top);
+			};
+		}
+	}
+}
+
+
+
 /*
-Example:
-<datalist id="browsers">
-  <option value="Internet Explorer">
-  <option value="Firefox">
-  <option value="Google Chrome">
-</datalist>
+Build the profil list like below.
+Use global var profil_db
+
+<div class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
 */
-function buildProfilList(input_origin){
-    var html_list = document.createElement('datalist');
-    html_list.id = id_list;
-    
-    var opt = document.createElement('option');
-    opt.value = profil_db.get_display_value_string(4);
-    opt.profil_id = 4;
+function buildProfilList() {
+	var html_list = document.createElement('div');
+	html_list.id = id_list;
+	html_list.className = "dropdown-content";
 
-    opt.onmouseover = function(evt){
-        console.log("Préaffichage du profil " + 4 + " : opt.value");
-    }
+	//Build dynamic list from profil front db
+	for (var id_profil in profil_db.profil_values) {
 
-    opt.onclick = function(evt){
-        console.log("choix du profil " + 4 + " : opt.value");
-        input_origin.value = "ma bite";
-    }
+	}
+	var opt = document.createElement('a');
+	opt.innerHTML = profil_db.get_display_value_string(4);
+	opt.setAttribute("profil_id", 4);
+	opt.href = "#";
 
-    html_list.appendChild(opt);
+	opt.onmouseover = function (evt) {
+		console.log("Préaffichage du profil " + 4 + " : opt.value");
+		fill_fields_v6(4);
+	}
 
-    //If we leave the list, remove it
-    html_list.onmouseleave = function(evt){
-        alert("leave list");
-        input_origin.removeAttribute("list");
-        html_list.remove();
-    }
-    return html_list;
+	opt.onclick = function (evt) {
+		console.log("choix du profil " + 4 + " : " + opt.innerHTML);
+	}
+
+	html_list.onmouseleave = function (evt) {
+		console.log("leave list");
+		html_list.style.display = "none";
+		clear_inputs();
+	}
+
+	html_list.appendChild(opt);
+
+	return html_list;
 }
