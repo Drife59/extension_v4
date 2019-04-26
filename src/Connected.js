443b08795261d894a941ddd418118cec466c5163
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-04-26 10:36:01 +0200

    Replace welcome image by button create profil

--- src/Connected.js
+++ src/Connected.js
@@ -4,20 +4,37 @@ import React, { Component } from 'react';
 
 import { FooterContact } from './FooterContact';
 
+import { config } from './config';
+
 export class Connected extends Component {
 
     constructor(props){
         super(props);
+        this.openProfil = this.openProfil.bind(this);
+
+    }
+
+    openProfil(e){
+        chrome.tabs.create({'url': chrome.extension.getURL(config.url_create_profil)}, function(tab) {
+            // Tab opened.
+        });
     }
 
     render() {
         return(
-            <section>
-                <div id="main" class="col-12">
-                    <div id="voice">
-                        <img alt="img_connected" id="img_connected" src="images/need_you.png" />
+            <section id="main">
+                <div id="connected" class="container">
+                    <div class="row">
+                        <div class="col-1"></div>
+                        <div class="col-10">
+                            <a class="button signup" onClick={this.openProfil}> Create profil </a>
+                        </div>
+                        <div class="col-1"></div>
+
+                        <div class="col-12">
+                            <p> Hey {this.props.getUser()}, we need you ! </p>
+                        </div>
                     </div>
-                    <p> Hey {this.props.getUser()}, we need you ! </p>
                 </div>
                 <FooterContact />
             </section>
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const config = {
     endpoint_back: "http://localhost:1665",
+    url_create_profil: "https://corail.me/step1.html",
     name: "Benjamin"
 }
