f05a12c60a094552bb36284dc86bb50d52b38468
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-05-24 10:20:46 +0200

    Implement find login form

--- public/lib/login/graphical.js
+++ public/lib/login/graphical.js
@@ -52,29 +52,33 @@ function nb_keyword_in_form(form, words){
 }
 
 
-//Look for login form in current web page
-function get_login_form(){
+// Look for login form in current web page
+// Return a list of eligible login form
+// Note(BG): on the contrary as I thought, this is not enough 
+// to find the good form in all cases. 
+// We need to check later on login and password
+function get_logins_form(){
 	var forms = document.body.querySelectorAll("form");
 
-	var login_form = null;
+	var login_form_list = [];
 	for (var i = 0; i < forms.length; i++) {
 		var current_form = forms[i];
-		var id_form = current_form.getAttribute("id");
-		var name_form = current_form.getAttribute("name");
-		var action_form = current_form.getAttribute("action");
 
 		var keyword_occurence = nb_keyword_in_form(current_form, form_keywords);
 		console.info("Found " + keyword_occurence + " keyword occurence for current form");
 		
 		if(keyword_occurence >= keyword_occurence_needed){
-			console.info("Found the login form in page");
-			console.info("Action: " + action_form);
-			console.info("id: " + id_form);
-			console.info("name: " + name_form);
-			return current_form;
+			console.info("Adding in list the login form in page, which is as follow:");
+			console.info("Action: " + current_form.action);
+			console.info("id: " + current_form.id);
+			console.info("name: " + current_form.name);
+
+			login_form_list.push(current_form);
 		}
 	}
-	return false;
+
+	console.info("Number of eligible login form found: " + login_form_list.length);
+	return login_form_list;
 }
 
 //This should always be provided a login form.
@@ -82,7 +86,6 @@ function get_login_form(){
 function get_password_field(login_form){
 	var password_field = login_form.querySelector("input[type=password]");
 	if( password_field == null){
-		console.warn("get_password_field: cannot find password field in provided form");
 		return false;
 	}
 	console.info("id of password field: " + password_field.id);
@@ -102,11 +105,37 @@ function get_login_field(login_form){
 	}
 
 	if(login_field == null){
-		console.warn("Cannot find login field in provided form");
 		return false;
 	}
 	console.info("id of login field: " + login_field.id);
 	console.info("name of login field: " + login_field.name);
 	return login_field;
+}
+
+// Research in current page the login form
+// Three conditions: 
+// 		 - there are forms eligible
+// 		 - There is a login field
+//		 - There is a password field
+function research_login_form(){
+	var forms_list = get_logins_form();
+
+	for(var i=0 ; i<forms_list.length ; i++){
+		var current_form = forms_list[i];
+
+		var current_login = get_login_field(current_form);
+		var current_password = get_password_field(current_form);
+
+		if(current_login != false && current_password != false){
+			console.info("[research_login_form]: Found the login form");
+			console.info("Action: " + current_form.action);
+			console.info("id: " + current_form.id);
+			console.info("name: " + current_form.name);
+
+			current_login.value = "moncul";
+			current_password.value = "mabite";
+			return current_form;
+		}
+	}
 }
 
