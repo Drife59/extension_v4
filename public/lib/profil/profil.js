ba9541c258a626c8031fd1d88ca21bdcec957e65
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-05-29 14:56:18 +0200

    Improve log

--- public/lib/profil/profil.js
+++ public/lib/profil/profil.js
@@ -415,7 +415,8 @@ class UserProfil {
             }
         }
 
-        console.info("User profil has been initiated with following content: " + JSON.stringify(this.profil_values, null,4));
+        console.info("[build_profil_from_json] User profil has been initiated with following content: " 
+            + JSON.stringify(this.profil_values, null,4));
     }
 
     async add_value_to_profil(email, pivot, value_text, profil_id, callback){
