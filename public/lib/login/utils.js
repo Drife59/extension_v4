00fd5e49bd30929bd56da074bfd3070279a4dc7b
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-06-01 16:13:29 +0200

    Delete dev log

--- public/lib/login/utils.js
+++ public/lib/login/utils.js
@@ -11,8 +11,6 @@ Define transversal function for login.
 // Be careful, in order for this to work,
 // the loggin detection and parsing must have been done
 function is_login_field(field){
-    console.warn("Field id: " + field.id);
-    console.warn("Is a login field ? " + field.hasAttribute(CODE_LOGIN_FIELD));
     return field.hasAttribute(CODE_LOGIN_FIELD);
 }
 
