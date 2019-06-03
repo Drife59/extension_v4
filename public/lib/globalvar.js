6d1e0f3e60f5e4f743b9cef11f4dcb5b7d1ad6fd
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-06-03 11:15:05 +0200

    Delete wrong statement in global_var.js

--- public/lib/globalvar.js
+++ public/lib/globalvar.js
@@ -8,11 +8,6 @@ globalvar.js
 Define globalvar to be set prior to application launched.
 */
 
-//Temps d'attente en ms avant remplissage des champs lors du chargement de page
-//Attention: un temps d'attente trop court risque de faire échouer le lancement
-//de l'app, pour cause de variable globale non-connue
-var timeout_parsing = 1500;
-
 
 var inputs = null;
 var selects = [];
