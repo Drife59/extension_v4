eebbbca57f5e3e3398dadbe6fd39be02d297cdf1
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-07-04 10:50:15 +0200

    If no login is available, let profil handle field

--- public/lib/login/graphical.js
+++ public/lib/login/graphical.js
@@ -195,13 +195,14 @@ function init_event_login_list(){
 	// Don't need this anymore, using global var 
 	//var {current_form:login_form, current_login:login_field, current_password:password_field} = result_research_login_form;
 
-	initialise_login_DOM();
-	bind_login_event();
-
+	//If there is no login at all, let profil function manage these fields
 	if(! login_front_db.has_login()){
 		console.debug("No login at all for website, aborting login initialisation");
 		return false;
 	}
+
+	initialise_login_DOM();
+	bind_login_event();
 
 	if( login_front_db.has_only_one_login()){
 		console.info("Only one login / psd was found for this domain.");
