a65750221793d937417c1f959810f4d3012ee32a
benjamin.grassart <benjamin.grassart@thalesgroup.com>
2019-07-08 12:07:05 +0200

    New key word for login detection

--- public/config/main_config.js
+++ public/config/main_config.js
@@ -197,13 +197,18 @@ CAREFUL: Include only word with lower case letter !
 */
 
 //Will look url for these key word
-var url_keywords = ["login", "log-in", "log_in", "connexion", "signin", "sign-in", "sign_in"];
+var url_keywords = ["login", "log-in", "log_in", 
+    "connexion", "connection", 
+    "signin", "sign-in", "sign_in"];
 
 //Will look in id and action var for these key word
-var form_keywords = ["login", "log-in", "log_in", "authentication", "authentification", "signin", "sign-in", "sign_in"];
+var form_keywords = ["login", "log-in", "log_in",
+    "authentication", "authentification", 
+    "signin", "sign-in", "sign_in", 
+    "connexion", "connection"];
 
 //Keywords to identify login field
-var field_login_keywords = ["email", "login", "log-in", "log_in", "e-mail", "authentication", "authentification"];
+var field_login_keywords = ["email", "e-mail", "e_mail", "login", "log-in", "log_in", "authentication", "authentification"];
 
 //Keywords to identify password
 var field_password_keywords = ["password"];
